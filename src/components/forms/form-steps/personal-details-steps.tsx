7dead67f0b9c1bfe65ed0d72831b5e294a8f76de
adarshgupta404 <gadarsh747@gmail.com>
2025-08-21 14:32:06 +0530

    .

--- src/components/forms/form-steps/personal-details-steps.tsx
+++ src/components/forms/form-steps/personal-details-steps.tsx
@@ -79,7 +79,7 @@ export function PersonalDetailsStep({ control, register, errors, clearErrors }: 
             />
           </div>
           <div className="space-y-2">
-            <Label>Gender</Label>
+            <Label>Gender *</Label>
             <ControlledSelect
               name="gender"
               control={control}
